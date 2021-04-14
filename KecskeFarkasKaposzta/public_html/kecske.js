c71cad3ecb9e9428d2af6e85786757dfdd02410a
Bartucz <43059630+Bartucz@users.noreply.github.com>
2021-04-14 11:38:08 +0200

    addevent listener hiba javítása
    <= helyett < kell nyilván

--- KecskeFarkasKaposzta/public_html/kecske.js
+++ KecskeFarkasKaposzta/public_html/kecske.js
@@ -1,38 +1,38 @@
 function ID(nev) {
 
-  return document.getElementById(nev);
+    return document.getElementById(nev);
 
 
 }
 
 function $(nev) {
 
-  return document.querySelectorAll(nev);
+    return document.querySelectorAll(nev);
 
 }
 
 
-var csonak=[];
+var csonak = [];
 
 
-function csonakba () {
+function csonakba() {
     //3. feladat
     csonak.push(this.src);
     console.log(csonak);
-    
+
     //7.feladat:
-    this.style.display="none";
-    
-    ID("csonak").innerHTML +='<img src="'+this.src+'" alt="utas" style="position: relative; top:130px; right: 0; width: 100px; height:100px; z-index: 5;">';
+    this.style.display = "none";
+
+    ID("csonak").innerHTML += '<img src="' + this.src + '" alt="utas" style="position: relative; top:130px; right: 0; width: 100px; height:100px; z-index: 5;">';
 }
-function csonakbolJobbra(){
-    
-    
+function csonakbolJobbra() {
+
+
 }
 
-function csonakbolBalra(){
-    
-    
+function csonakbolBalra() {
+
+
 }
 
 
@@ -44,25 +44,22 @@ function init() {
     
     
     
+
+    for (var i = 0; i < $("aside img").length; i++) {
+
+        $("#bal img")[i].addEventListener("click", csonakba);
     
-
-for (var i = 0; i <= $("aside img").length; i++) {
-
-    $("#bal img")[i].addEventListener("click", csonakba);
-    
-    
-
-  }
+      }
 
 
 
 
 
-  ;
+  
 }
 
 
 
 
 
-window.addEventListener("load", init, false);
+
